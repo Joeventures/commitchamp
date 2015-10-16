1d726ce1816f0c6239eafec2908bf0bc9962eb55
Joe Winter <joe@joeventures.com>
2015-10-16 10:55:14 -0400

    Initial Setup

--- lib/commitchamp.rb
+++ lib/commitchamp.rb
@@ -6,12 +6,55 @@ require "commitchamp/version"
 
 module Commitchamp
   class App
+    include HTTParty
+    base_uri "https://api.github.com"
     def initialize
     end
 
     def run
-      # Your code goes here...
+      until sort_another == 'N' do
+        get_auth_token
+        @repo_name = get_repo_name
+        @contributions = get_repo_contributions
+        @contributions = sort_repo_contributions
+        show_sorted_repo_contributions
+        puts "Would you like to get another list of contributions?"
+        puts "Enter \"N\" if no, or any other key for yes."
+        sort_another = gets.chomp.upcase
+      end
+
     end
+
+    private
+
+    def get_auth_token
+      # prompt for the auth token and set @auth
+      puts "Please enter your auth token: "
+      auth_token = gets.chomp
+      @auth = {
+          "Authorization" => "token #{auth_token}",
+          "User-Agent"    => "HTTParty"
+      }
+    end
+
+    def get_repo_name
+      # prompt for and return repo name
+    end
+
+    def get_repo_contributions
+      # return a hash of all the repo contributions
+    end
+
+    def sort_repo_contributions
+      # prompt for what to sort by
+      # sort the repo contributions
+      # return a hash of the contributions, properly sorted
+    end
+
+    def show_sorted_repo_contributions
+      # puts the sorted list of contributions
+    end
+
   end
 end
 
