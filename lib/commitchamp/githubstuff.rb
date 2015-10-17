53424e5d25c1ad365adcaea6d4d1a1546f0cd4b5
Joe Winter <joe@joeventures.com>
2015-10-17 17:56:52 -0400

    Added most of the normal mode functionality.

--- lib/commitchamp/githubstuff.rb
+++ lib/commitchamp/githubstuff.rb
@@ -1,6 +1,9 @@
 module Commitchamp
   class GitHubStuff
 
+    include HTTParty
+    base_uri "https://api.github.com"
+
     def initialize(auth_token)
       @auth = {
           "Authorization" => "token #{auth_token}",
@@ -8,12 +11,18 @@ module Commitchamp
       }
     end
 
-    def get_repo_name
+    def get_repo_info
       # prompt for and return repo name
+      puts "What is the name of the repository you would like to \"hack\"?"
+      @repo_name = gets.chomp
+      puts "And who is the unfortunate owner of this repo?"
+      @repo_owner = gets.chomp
     end
 
     def get_repo_contributions
       # return a hash of all the repo contributions
+      GitHubStuff.get( "/repos/#{@repo_owner}/#{@repo_name}/stats/contributors",
+        :headers => @auth)
     end
 
     def sort_repo_contributions
